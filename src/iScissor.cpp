3b710ff62a4c7f877d171d3317126ba022ae5ce8
Zheng Fan <fan0114@gmail.com>
2013-09-09 18:48:59 -0400

    implemented LiveWireDP
    todo: test priority queue operator
    make standard position number
    find out if malloc is needed on r[8]

--- src/iScissor.cpp
+++ src/iScissor.cpp
@@ -1,8 +1,11 @@
 /* iScissor.cpp */
 /* Main file for implementing project 1.  See TODO statments below
- * (see also correlation.cpp and iScissor.h for additional TODOs) */
+* (see also correlation.cpp and iScissor.h for additional TODOs) */
 
 #include <assert.h>
+#include <queue>
+#include <vector>
+using namespace std;
 
 #include "correlation.h"
 #include "iScissor.h"
@@ -13,129 +16,178 @@ const double linkLengths[8] = { 1.0, SQRT2, 1.0, SQRT2, 1.0, SQRT2, 1.0, SQRT2 }
 
 inline Node& NODE(Node* n, int i, int j, int width)
 {
-    return *(n + j * width + i);
+	return *(n + j * width + i);
 }
 
 inline unsigned char PIXEL(const unsigned char* p, int i, int j, int c, int width)
 {
-    return *(p + 3 * (j * width + i) + c);
+	return *(p + 3 * (j * width + i) + c);
 }
 
 /************************ TODO 1 ***************************
- *InitNodeBuf
- *	INPUT:
- *		img:	a RGB image of size imgWidth by imgHeight;
- *		nodes:	a allocated buffer of Nodes of the same size, one node corresponds to a pixel in img;
- *  OUPUT:
- *      initializes the column, row, and linkCost fields of each node in the node buffer.
- */
+*InitNodeBuf
+*	INPUT:
+*		img:	a RGB image of size imgWidth by imgHeight;
+*		nodes:	a allocated buffer of Nodes of the same size, one node corresponds to a pixel in img;
+*  OUPUT:
+*      initializes the column, row, and linkCost fields of each node in the node buffer.
+*/
 
 void InitNodeBuf(Node* nodes, const unsigned char* img, int imgWidth, int imgHeight)
 {
-printf("lolol TODO: %s:%d\n", __FILE__, __LINE__); 
+	//printf("lolol TODO: %s:%d\n", __FILE__, __LINE__); 
+
 
 }
 /************************ END OF TODO 1 ***************************/
 
 static int offsetToLinkIndex(int dx, int dy)
 {
-    int indices[9] = { 3, 2, 1, 4, -1, 0, 5, 6, 7 };
-    int tmp_idx = (dy + 1) * 3 + (dx + 1);
-    assert(tmp_idx >= 0 && tmp_idx < 9 && tmp_idx != 4);
-    return indices[tmp_idx];
+	int indices[9] = { 3, 2, 1, 4, -1, 0, 5, 6, 7 };
+	int tmp_idx = (dy + 1) * 3 + (dx + 1);
+	assert(tmp_idx >= 0 && tmp_idx < 9 && tmp_idx != 4);
+	return indices[tmp_idx]; 
 }
 
 /************************ TODO 4 ***************************
- *LiveWireDP:
- *	INPUT:
- *		seedX, seedY:	seed position in nodes
- *		nodes:			node buffer of size width by height;
- *      width, height:  dimensions of the node buffer;
- *		selection:		if selection != NULL, search path only in the subset of nodes[j*width+i] if selection[j*width+i] = 1;
- *						otherwise, search in the whole set of nodes.
- *		numExpanded:		compute the only the first numExpanded number of nodes; (for debugging)
- *	OUTPUT:
- *		computes the minimum path tree from the seed node, by assigning
- *		the prevNode field of each node to its predecessor along the minimum
- *		cost path from the seed to that node.
- */
+*LiveWireDP:
+*	INPUT:
+*		seedX, seedY:	seed position in nodes
+*		nodes:			node buffer of size width by height;
+*      width, height:  dimensions of the node buffer;
+*		selection:		if selection != NULL, search path only in the subset of nodes[j*width+i] if selection[j*width+i] = 1;
+*						otherwise, search in the whole set of nodes.
+*		numExpanded:		compute the only the first numExpanded number of nodes; (for debugging)
+*	OUTPUT:
+*		computes the minimum path tree from the seed node, by assigning
+*		the prevNode field of each node to its predecessor along the minimum
+*		cost path from the seed to that node.
+*/
+
+struct Comparator
+{
+	bool operator()(const Node* left, const Node* right)
+	{
+		return left->totalCost > right->totalCost;
+	}
+};
 
 void LiveWireDP(int seedX, int seedY, Node* nodes, int width, int height, const unsigned char* selection, int numExpanded)
 {
-printf("TODO: %s:%d\n", __FILE__, __LINE__); 
+	//printf("TODO: %s:%d\n", __FILE__, __LINE__); 
+	priority_queue<Node*,vector<Node*>,Comparator> pq;
+	int x,y;
+	for (x=0;x<width;x++){
+		for(y=0;y<height;y++){
+			nodes[width*y+x].state=INITIAL;
+		}
+	}
+	nodes[width*seedY+seedX].totalCost=0;
+	pq.push(&(nodes[width*seedY+seedX]));
+	while(!pq.empty()){
+		Node* q = pq.top();
+		pq.pop();
+		q->state=EXPANDED;
+		int qy=q->row;
+		int qx=q->column;
+		Node* r[8]; 
+		r[3] = &nodes[width*(qy-1)+qx-1];
+		r[2] = &nodes[width*(qy-1)+qx];
+		r[1] = &nodes[width*(qy-1)+qx+1];
+		r[4] = &nodes[width*(qy)+qx-1];
+		r[0] = &nodes[width*(qy)+qx+1];
+		r[5] = &nodes[width*(qy+1)+qx-1];
+		r[6] = &nodes[width*(qy+1)+qx];
+		r[7] = &nodes[width*(qy+1)+qx+1];
+		int i;
+		for(i=0;i<8;i++){
+			if((r[i]->state)!=EXPANDED){
+				if((r[i]->state)==INITIAL){
+					r[i]->totalCost = (q->totalCost) + (q->linkCost[i]);
+					pq.push(r[i]);
+					r[i]->state=ACTIVE;
+				}else if((r[i]->state)==ACTIVE){
+					double newCost = (q->totalCost) + (q->linkCost[i]);
+					if(newCost < (r[i]->totalCost)){
+						r[i]->totalCost = newCost;
+					}
+				}
+			}
+		}
+	}
 
 }
 /************************ END OF TODO 4 ***************************/
 
 /************************ TODO 5 ***************************
- *MinimumPath:
- *	INPUT:
- *		nodes:				a node buffer of size width by height;
- *		width, height:		dimensions of the node buffer;
- *		freePtX, freePtY:	an input node position;
- *	OUTPUT:
- *		insert a list of nodes along the minimum cost path from the seed node to the input node.
- *		Notice that the seed node in the buffer has a NULL predecessor.
- *		And you want to insert a *pointer* to the Node into path, e.g.,
- *		insert nodes+j*width+i (or &(nodes[j*width+i])) if you want to insert node at (i,j), instead of nodes[nodes+j*width+i]!!!
- *		after the procedure, the seed should be the head of path and the input code should be the tail
- */
+*MinimumPath:
+*	INPUT:
+*		nodes:				a node buffer of size width by height;
+*		width, height:		dimensions of the node buffer;
+*		freePtX, freePtY:	an input node position;
+*	OUTPUT:
+*		insert a list of nodes along the minimum cost path from the seed node to the input node.
+*		Notice that the seed node in the buffer has a NULL predecessor.
+*		And you want to insert a *pointer* to the Node into path, e.g.,
+*		insert nodes+j*width+i (or &(nodes[j*width+i])) if you want to insert node at (i,j), instead of nodes[nodes+j*width+i]!!!
+*		after the procedure, the seed should be the head of path and the input code should be the tail
+*/
 
 void MinimumPath(CTypedPtrDblList <Node>* path, int freePtX, int freePtY, Node* nodes, int width, int height)
 {
-printf("TODO: %s:%d\n", __FILE__, __LINE__); 
+	printf("TODO: %s:%d\n", __FILE__, __LINE__); 
 
 }
 /************************ END OF TODO 5 ***************************/
 
 /************************ An Extra Credit Item ***************************
- *SeeSnap:
- *	INPUT:
- *		img:				a RGB image buffer of size width by height;
- *		width, height:		dimensions of the image buffer;
- *		x,y:				an input seed position;
- *	OUTPUT:
- *		update the value of x,y to the closest edge based on local image information.
- */
+*SeeSnap:
+*	INPUT:
+*		img:				a RGB image buffer of size width by height;
+*		width, height:		dimensions of the image buffer;
+*		x,y:				an input seed position;
+*	OUTPUT:
+*		update the value of x,y to the closest edge based on local image information.
+*/
 
 void SeedSnap(int& x, int& y, unsigned char* img, int width, int height)
 {
-    printf("SeedSnap in iScissor.cpp: to be implemented for extra credit!\n");
+	printf("SeedSnap in iScissor.cpp: to be implemented for extra credit!\n");
 }
 
 //generate a cost graph from original image and node buffer with all the link costs;
 void MakeCostGraph(unsigned char* costGraph, const Node* nodes, const unsigned char* img, int imgWidth, int imgHeight)
 {
-    int graphWidth = imgWidth * 3;
-    int graphHeight = imgHeight * 3;
-    int dgX = 3;
-    int dgY = 3 * graphWidth;
+	int graphWidth = imgWidth * 3;
+	int graphHeight = imgHeight * 3;
+	int dgX = 3;
+	int dgY = 3 * graphWidth;
 
-    int i, j;
-    for (j = 0; j < imgHeight; j++) {
-        for (i = 0; i < imgWidth; i++) {
-            int nodeIndex = j * imgWidth + i;
-            int imgIndex = 3 * nodeIndex;
-            int costIndex = 3 * ((3 * j + 1) * graphWidth + (3 * i + 1));
+	int i, j;
+	for (j = 0; j < imgHeight; j++) {
+		for (i = 0; i < imgWidth; i++) {
+			int nodeIndex = j * imgWidth + i;
+			int imgIndex = 3 * nodeIndex;
+			int costIndex = 3 * ((3 * j + 1) * graphWidth + (3 * i + 1));
 
-            const Node* node = nodes + nodeIndex;
-            const unsigned char* pxl = img + imgIndex;
-            unsigned char* cst = costGraph + costIndex;
+			const Node* node = nodes + nodeIndex;
+			const unsigned char* pxl = img + imgIndex;
+			unsigned char* cst = costGraph + costIndex;
 
-            cst[0] = pxl[0];
-            cst[1] = pxl[1];
-            cst[2] = pxl[2];
+			cst[0] = pxl[0];
+			cst[1] = pxl[1];
+			cst[2] = pxl[2];
 
-            //r,g,b channels are grad info in seperate channels;
-            DigitizeCost(cst	   + dgX, node->linkCost[0]);
-            DigitizeCost(cst - dgY + dgX, node->linkCost[1]);
-            DigitizeCost(cst - dgY      , node->linkCost[2]);
-            DigitizeCost(cst - dgY - dgX, node->linkCost[3]);
-            DigitizeCost(cst	   - dgX, node->linkCost[4]);
-            DigitizeCost(cst + dgY - dgX, node->linkCost[5]);
-            DigitizeCost(cst + dgY	   ,  node->linkCost[6]);
-            DigitizeCost(cst + dgY + dgX, node->linkCost[7]);
-        }
-    }
+			//r,g,b channels are grad info in seperate channels;
+			DigitizeCost(cst	   + dgX, node->linkCost[0]);
+			DigitizeCost(cst - dgY + dgX, node->linkCost[1]);
+			DigitizeCost(cst - dgY      , node->linkCost[2]);
+			DigitizeCost(cst - dgY - dgX, node->linkCost[3]);
+			DigitizeCost(cst	   - dgX, node->linkCost[4]);
+			DigitizeCost(cst + dgY - dgX, node->linkCost[5]);
+			DigitizeCost(cst + dgY	   ,  node->linkCost[6]);
+			DigitizeCost(cst + dgY + dgX, node->linkCost[7]);
+		}
+	}
 }
 
